72ba4417e306e4df1924375cb1d3c8227e8f2fa9
IskanderBouslimi <bouslimiskandar@gmail.com>
2026-03-14 18:03:09 +0100

    ajout date_execution + push notifications 2

--- models/Tache.js
+++ models/Tache.js
@@ -5,9 +5,8 @@ const TacheSchema = new mongoose.Schema({
     description: { type: String },
     terminee: { type: Boolean, default: false },
     dateCreation: { type: Date, default: Date.now },
+    date_execution: { type: Date, default: null },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 module.exports = mongoose.model('Tache', TacheSchema);
-
-
